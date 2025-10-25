9b647828bf353b643d23e01a6612b1640f2c3b30
sakanul <lukasmoreira@ufam.edu.br>
2025-10-24 20:22:08 -0400

    feat: Create a encrypter password

--- src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -1,4 +1,5 @@
 ﻿using Mapster;
+using MyRecipeBook.Application.Services.Cryptography;
 using MyRecipeBook.Application.Services.Mappings;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
@@ -11,16 +12,20 @@ namespace MyRecipeBook.Application.UseCases.User.Register
         /* Regra de negócio para registrar um usuário */
         public ResponseResgisteredUserJson Execute(RequestRegisterUserJson request)
         {
+            /* Configurar os mapeamentos */
+            MapConfigurations.Configure(); /* Ainda falta fazer a injeção de dependencias */
+
+            /* Instanciar o encriptador de senhas */
+            var passwordEncrypter = new PasswordEncrypter();
+
             /* Validar a request */
             ValidateRequest(request);
 
             /* Mapear a request para a entidade */
-            MapConfigurations.Configure(); /* Ainda falta fazer a injeção de dependencias */
             var user = request.Adapt<Domain.Entities.User>();
 
             /* Criptografar a senha */
-
-
+            user.Password = passwordEncrypter.Encrypt(request.Password);
 
             /* Salvar a entidade no banco de dados */
 
